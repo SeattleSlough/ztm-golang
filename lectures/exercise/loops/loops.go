bb76fe742fb845f81e44f2d6b6c4101b5c32e143
Michael Maggs <seattleslew@runawayserver.com>
2025-08-09 18:01:31 -0700

    loops

--- lectures/exercise/loops/loops.go
+++ lectures/exercise/loops/loops.go
@@ -2,10 +2,6 @@
 //  Implement the classic "FizzBuzz" problem using a `for` loop.
 //
 //--Requirements:
-//* Print integers 1 to 50, except:
-//  - Print "Fizz" if the integer is divisible by 3
-//  - Print "Buzz" if the integer is divisible by 5
-//  - Print "FizzBuzz" if the integer is divisible by both 3 and 5
 //
 //--Notes:
 //* The remainder operator (%) can be used to determine divisibility
@@ -15,5 +11,39 @@ package main
 import "fmt"
 
 func main() {
+	//* Print integers 1 to 50, except:
+	// for i := 1; i <= 50; i++ {
+	// 	//  - Print "FizzBuzz" if the integer is divisible by both 3 and 5
+		// 	if i % 3 == 0 && i % 5 == 0 {
+		// 		fmt.Println("FizzBuzz")
+		// 	} else if i % 3 == 0 {
+			// 		//  - Print "Fizz" if the integer is divisible by 3
+			// 		fmt.Println("Fizz")
+		// 	} else if i % 5 == 0 {
+				// 		//  - Print "Buzz" if the integer is divisible by 5
+				// 		fmt.Println("Buzz")
+		// 	} else {
+		// 		fmt.Println(i)
+	// 	}
+// }
+
+	for i := 1; i <= 50; i++ {
+		divisibleby3 := i%3 == 0
+		divisibleby5 := i%5 == 0
+
+		if divisibleby3 && divisibleby5 {
+			//  - Print "FizzBuzz" if the integer is divisible by both 3 and 5
+			fmt.Println("FizzBuzz")
+		} else if divisibleby3 {
+			// 		//  - Print "Fizz" if the integer is divisible by 3
+			fmt.Println("Fizz")
+		} else if divisibleby5 {
+	// 		//  - Print "Buzz" if the integer is divisible by 5
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i)
+		}
+
+	}
 }
 
