c5ed9797e965c2c469a2194a1136a7085d8de163
Michael Maggs <seattleslew@runawayserver.com>
2025-08-12 11:35:25 -0700

    slices and arrays

--- lectures/exercise/arrays/arrays.go
+++ lectures/exercise/arrays/arrays.go
@@ -3,22 +3,53 @@
 //  out information about the list.
 //
 //--Requirements:
-//* Using an array, create a shopping list with enough room
-//  for 4 products
-//  - Products must include the price and the name
-//* Insert 3 products into the array
-//* Print to the terminal:
-//  - The last item on the list
-//  - The total number of items
-//  - The total cost of the items
-//* Add a fourth product to the list and print out the
 //  information again
 
 package main
 
 import "fmt"
 
-func main() {
+//  - Products must include the price and the name
+type Products struct {
+	price int
+	name string
+}
 
+func printStats(list [4]Products) {
+	var cost, totalItems int
+
+	for i := 0; i < len(list); i++{
+		item := list[i]
+		cost += item.price
+
+		if item.name != "" {
+			totalItems += 1
+		}
+	}
+	//  - The total cost of the items
+	fmt.Println("The total cost is", cost)
+	//  - The total number of items
+	fmt.Println("The total number of items is", totalItems)
+	//  - The last item on the list
+	fmt.Println("The last item in the list is", list[totalItems - 1].name)
+}
+
+func main() {
+	//* Using an array, create a shopping list with enough room
+	//  for 4 products
+	//* Insert 3 products into the array
+	shoppingList := [4]Products{
+		{5, "oranges"},
+		{10, "steak"},
+		{4, "milk"},
+	}
+
+	//* Print to the terminal:
+	printStats(shoppingList)
+
+	//* Add a fourth product to the list and print out the
+	shoppingList[3]= Products{7, "bread"}
+
+	printStats(shoppingList)
 }
 
--- lectures/exercise/slices/slices.go
+++ lectures/exercise/slices/slices.go
@@ -1,23 +1,49 @@
-//--Summary:
-//  Create a program to manage parts on an assembly line.
-//
-//--Requirements:
-//* Using a slice, create an assembly line that contains type Part
-//* Create a function to print out the contents of the assembly line
-//* Perform the following:
-//  - Create an assembly line having any three parts
-//  - Add two new parts to the line
-//  - Slice the assembly line so it contains only the two new parts
-//  - Print out the contents of the assembly line at each step
-//--Notes:
-//* Your program output should list 3 parts, then 5 parts, then 2 parts
-
 package main
-
 import "fmt"
 
 type Part string
 
+//--Summary:
+//  Create a program to manage parts on an assembly line.
+//
+//--Requirements:
 func main() {
+	//* Using a slice, create an assembly line that contains type Part
+	//  - Create an assembly line having any three parts
+	assemblyline := make([]Part, 3)
+
+	assemblyline[0] = "Pipe"
+	assemblyline[1] = "Bolt"
+	assemblyline[2] = "Sheet"
+
+	fmt.Println("3 parts")
+	showLine(assemblyline)
+
+	//  - Add two new parts to the line
+	assemblyline = append(assemblyline, "Washer", "Wheel")
+	fmt.Println("\nAdded two parts:")
+	showLine(assemblyline)
+
+	//  - Slice the assembly line so it contains only the two new parts
+	assemblyline = assemblyline[3:]
+	fmt.Println("\nSliced")
+	showLine(assemblyline)
 }
+
+//* Perform the following:
+//  - Print out the contents of the assembly line at each step
+//--Notes:
+//* Your program output should list 3 parts, then 5 parts, then 2 parts
+
+
+
+
+//* Create a function to print out the contents of the assembly line
+func showLine(line []Part) {
+	for i := 0; i < len(line); i++ {
+		part := line[i]
+		fmt.Println(part)
+	}
+}
+
 
